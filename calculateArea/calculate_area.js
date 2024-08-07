6de8d50a2d8c48d547b42aeecd4f8948a274e744
Vu Anh Tuan <vu.anh.tuan@captcha.com>
2024-08-07 15:31:53 +0700

    write function

---
+++ calculateArea/calculate_area.js
@@ -0,0 +1,7 @@
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+
+    let area = length * width;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
